ba2af94798e5050dd495fb06bb4d9267077a4d84
minatoxkushina09-bit <minatoxkushina09@gmail.com>
2026-09-05 12:44:50 +0530

    Refactor search method in SearchExtractor
    Refactor search method to return a structured path based on provider ID.

--- src/extractors/search.extractor.js
+++ src/extractors/search.extractor.js
@@ -6,19 +6,11 @@ class SearchExtractor extends SiteExtractor {
   }
 
   async search(query) {
-    const searchPath =
-      this.base.providerId === 'animelok'
-        ? `/search?keyword=${encodeURIComponent(query)}`
-        : `/?s=${encodeURIComponent(query)}`;
-
-    const { $ } = await this.page(searchPath);
-
-    const selectors =
-      this.base.providerId === 'animelok'
-        ? 'article, .anime-item, .item, .search-item'
-        : 'article, .post, .search-item, .item';
-
-    return this.list($, selectors);
+    return {
+      path: this.base.providerId === 'animelok'
+        ? `/search/${encodeURIComponent(query)}`
+        : `/search?query=${encodeURIComponent(query)}`
+    };
   }
 
   async searchFullPage(query) {
